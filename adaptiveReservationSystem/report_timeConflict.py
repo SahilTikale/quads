537249981d4d30a41fa92163022745055fd41e74
SahilTikale <sahil.tikale@gmail.com>
2018-08-01 22:13:58 -0400

    Reports the longest sequence of non-conflicting jobs and longest sequence of jobs with max utilization.

--- adaptiveReservationSystem/report_timeConflict.py
+++ adaptiveReservationSystem/report_timeConflict.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 import sys
 from operator import itemgetter
 from pathlib2 import Path
@@ -24,45 +25,114 @@ else:
 newlist = sorted(sch_req_list, key=itemgetter('end'))
 
 print "Input list as obtained."
-
+print " "
 for i in sch_req_list:
     print i
-
+print " "
 print " Input list sorted by end time. "
+print " "
 
 for i in newlist:
     print i
 
-for i in newlist:
-    print newlist.index(i)
+#for i in newlist:
+#    print newlist.index(i)
+
+
+print " "
+print " ********************************"
+print " "
 
 sol_list = []
+conflict_list = []
 
 for outer in newlist:
-    outer_req_no = newlist.index(i)
-    if outer_req_no == 0:
-        # If the item is first in new list put it in its own solution list.
-        sol = [outer_req_no]
-        sol_list.append(sol)
-    else:
-        # create an end time object from list to key 'end'
-        outer_endtime_input = dict(
-        year=outer['end'][0], month=outer['end'][1], day=outer['end'][2],
-        hour=outer['end'][3], minute=outer['end'][4]
+    #outer_req_no = newlist.index(outer)
+    outer_begintime_input = dict(
+        year=outer['begin'][0],
+        month=outer['begin'][1],
+        day=outer['begin'][2]
     )
-        outer_endtime = datetime.datetime(**outer_endtime_input)
-        for inner in newlist:
-            inner_req_no = newlist.index(i)
+    outer_begintime = datetime.datetime(**outer_begintime_input)
+    #no_conflict = []
+    conflict = []
+    for inner in newlist:
+        no_conflict = []
+        if(newlist.index(inner) < newlist.index(outer)):
+            #inner_req_no = newlist.index(inner)
+            #print inner
             inner_endtime_input = dict(
-                year=inner['end'][0], month=inner['end'][1],
+                year=inner['end'][0],
+                month=inner['end'][1],
                 day=inner['end'][2],
-                hour=inner['end'][3], minute=inner['end'][4]
             )
-	    inner_endtime =	datetime.datetime(**inner_endtime_input)
-        
-        
-        
-        
+            inner_endtime = datetime.datetime(**inner_endtime_input)
+            #print "comparing endtime of job {}: {} to begin time of job {}: {}".format(
+            #    newlist.index(inner), inner_endtime, newlist.index(outer), outer_begintime)
+            if (inner_endtime <= outer_begintime):
+                print "comparing endtime of job {}: {} to begin time of job {}: {} No time conflict".format(
+                newlist.index(inner), inner_endtime, newlist.index(outer), outer_begintime)
+                no_conflict.append(newlist.index(inner))
+                no_conflict.append(newlist.index(outer))
+            else:
+                print " "
+                print "comparing endtime of job {}: {} to begin time of job {}: {} CONFLICTED".format(
+                newlist.index(inner), inner_endtime, newlist.index(outer), outer_begintime)
+                conflict.append(newlist.index(inner))
+            if len(no_conflict) > 0:
+                sol_list.append(no_conflict)
+                
+#    no_conflict.append(newlist.index(outer))
+    if len(conflict) > 0:
+        conflict.append(newlist.index(outer))
+#    sol_list.append(no_conflict)
+    conflict_list.append(conflict)
 
+result = []
+for i in sol_list:
+    x = 0
+    for j in sol_list:
+        if(sol_list.index(j) < sol_list.index(i)) and j[1] == i[0]:
+            result.append(j + list(set(i) -set(j)))
+            x = x+1
+        if x == 0 and i not in result:
+            result.append(i)
+
+# Finding longest sequence of jobs that can be scheduled without time conflict.
+result.sort(key=len)
+
+# Finding longest sequence of jobs that maximize utilization.
+
+max_duration = 0
+max_duration_jobs = []
+for i in result:
+    total_duration = 0
+    for j in i:
+        total_duration += int(newlist[j]['duration'])
+
+    print (i, total_duration)
+
+    if total_duration > max_duration:
+        max_duration = total_duration
+        max_duration_jobs = [(i, total_duration)]
+    elif total_duration == max_duration and (i, total_duration) not in max_duration_jobs:
+        max_duration_jobs.append((i, total_duration))
+
+
+
+
+print " *********"
+print " "
+print "Longest sequence of jobs that can be scheduled: {}".format(result[-1])
+print " "
+print " "
+print "Longest sequence of jobs with max cluster utilization: {}".format(max_duration_jobs)
+        
     
     
+print " "
+print " "
+print "No conflict list: {}".format(sol_list)
+print "Conflict list: {}".format(conflict_list)
+print "Result: {}".format(result)
+
